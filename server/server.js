07598f7d618613769d56d70433297300c934b96c
Kaynath Zehra Rizvi <rizvikaynath@gmail.com>
2025-02-19 16:07:04 +0530

    Remove shared folder

--- server/server.js
+++ server/server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-const products = require('../shared/products');  // Import shared product data
+const products = require('./products');  // Import shared product data
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 // Serve static images from the 'shared/images' folder
-app.use('/images', express.static(path.join(__dirname, '../shared/images')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 // Define API route to get product data
 app.get('/api/products', (req, res) => {
