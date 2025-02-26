8a2c85a4a5aaf0b034104557e21a2f8523254e2c
Kaynath Zehra Rizvi <rizvikaynath@gmail.com>
2025-02-26 17:18:30 +0530

    updated index.js

--- server/index.js
+++ server/index.js
@@ -34,6 +34,13 @@ app.get('/api/products', async (req, res) => {
   }
 });
 
+// Serve the React build files for any other requests
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 const PORT = process.env.PORT || 8081;
 app.listen(PORT, () => {
   console.log(`Backend running on http://localhost:${PORT}`);
