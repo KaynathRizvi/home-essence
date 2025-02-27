4ffb5a8b416bad92fe84c3517055453ca9aa4c1f
Kaynath Zehra Rizvi <rizvikaynath@gmail.com>
2025-02-28 01:39:42 +0530

    responsive design updated

--- client/src/components/ProductCatalog/ProductCatalog.js
+++ client/src/components/ProductCatalog/ProductCatalog.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import './ProductCatalog.css';
 import ProductSearchBar from '../ProductSearchBar/ProductSearchBar';
 import Category from '../Category/Category';
