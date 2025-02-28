e338b394fc201022ba60cbb4de14ef1c96a84fab
Kaynath Zehra Rizvi <rizvikaynath@gmail.com>
2025-02-28 16:15:12 +0530

    added foreign key

--- server/models/Cart.js
+++ server/models/Cart.js
@@ -1,29 +1,36 @@
 // models/Cart.js
 module.exports = (sequelize, DataTypes) => {
-    const Cart = sequelize.define('Cart', {
-      cart_id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-      },
-      user_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-      },
-      product_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-      },
-      quantity: {
-        type: DataTypes.INTEGER,
-        defaultValue: 1,
-      },
-    }, {
-      tableName: 'cart_item',
-      timestamps: true,
-      freezeTableName: true
-    });
-  
-    return Cart;
-  };
-  
+  const Cart = sequelize.define('Cart', {
+    cart_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'user_accounts',  // The target model/table name
+        key: 'user_id'           // The primary key in that table
+      }
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'products',       // The target model/table name
+        key: 'product_id'        // The primary key in that table
+      }
+    },
+    quantity: {
+      type: DataTypes.INTEGER,
+      defaultValue: 1,
+    },
+  }, {
+    tableName: 'cart_item',
+    timestamps: true,
+    freezeTableName: true
+  });
+
+  return Cart;
+};
